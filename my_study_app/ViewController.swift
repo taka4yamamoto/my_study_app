14b6e20d2ac3d345936d30a8e8bf208c3d2405d5
taka4yamamoto <tksymmt.ymtk@gmail.com>
2018-07-06 21:09:06 +0900

    initialize

---
+++ my_study_app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  my_study_app
+//
+//  Created by A10 lab Inc. 006 on 2018/07/06.
+//  Copyright © 2018 yamataka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
