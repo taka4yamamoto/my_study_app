3d0282a691a07cca97f3ba6db75f50ae4b378e22
taka4yamamoto <tksymmt.ymtk@gmail.com>
2019-02-09 18:07:40 +0900

    feature: set transition to CustomCellTableView

---
+++ my_study_app/ViewControllers/CustomCellTableView/CustomCellTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CustomCellTableViewController.swift
+//  my_study_app
+//
+//  Created by taka4yamamoto on 2019/02/09.
+//  Copyright © 2019 yamataka. All rights reserved.
+//
+
+import UIKit
+
+class CustomCellTableViewController: UITableViewController {
+}
+
